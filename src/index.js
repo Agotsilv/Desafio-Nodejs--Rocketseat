ee50bab179b88aee120dc9a48cc2cb9abc7d9f24
Tiago Oliveira <94474189+Agotsilv@users.noreply.github.com>
2023-01-10 15:50:22 -0300

    Todo List Api

--- src/index.js
+++ src/index.js
@@ -1,29 +1,71 @@
-const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
+const express = require('express');
 const app = express();
+app.use(express.json())
+// app.use(express.json());
+// app.use(bodyParser.json());
 
-app.use(cors());
-app.use(express.json());
-
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+ const {username} = request.headers
+
+  const user = users.find((resp) => resp.username === username)
+
+  if(!user){
+    return response.status(400).json({message:  'Usuário já cadastrado!' })
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todo: []
+  })
+
+  const userExists = users.some((users) => users.username === username);
+
+  if(!userExists){
+    return response.status(400).json({message: 'Account already exists'});
+  }
+
+  return response.status(200).send()
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  const todos = user.todo;
+
+  return response.status(200).json({ todos: todos }).send();
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post('/todoCreated', checksExistsUserAccount, (request, response) => {
+  
+  const { user } = request;
+  const { title , deadline} = request.body
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    createdAt: new Date(),
+  }
+
+  user.todo.push(todo)
+  return response.status(200).json({ message: "Tarefa Cadastrada com sucesso!"}).send();
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
