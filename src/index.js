9684311a94d260f7e76fec05a2729752c17a2eb3
Tiago Oliveira <94474189+Agotsilv@users.noreply.github.com>
2023-01-11 11:07:18 -0300

    final update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const { v4: uuidv4 } = require('uuid');
-
 const express = require('express');
 const app = express();
+
 app.use(express.json())
-// app.use(express.json());
-// app.use(bodyParser.json());
 
 const users = [];
 
@@ -17,7 +14,7 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find((resp) => resp.username === username)
 
   if(!user){
-    return response.status(400).json({message:  'Usuário já cadastrado!' })
+    return response.status(400).json({message:  'User does not exist!' })
   }
 
   request.user = user
@@ -26,7 +23,6 @@ function checksExistsUserAccount(request, response, next) {
 
 app.post('/users', (request, response) => {
   const {name, username} = request.body;
-
   users.push({
     id: uuidv4(),
     name,
@@ -40,7 +36,7 @@ app.post('/users', (request, response) => {
     return response.status(400).json({message: 'Account already exists'});
   }
 
-  return response.status(200).send()
+  return response.status(200).json({message: 'successfully registered user!'}).send()
 
 });
 
@@ -53,7 +49,6 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  
   const { user } = request;
   const { title , deadline} = request.body
   const todo = {
@@ -64,23 +59,54 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
     createdAt: new Date(),
   }
 
-
   user.todo.push(todo)
-  return response.status(200).json({ message: "Tarefa Cadastrada com sucesso!"}).send();
+  return response.status(200).json({ message: "job successfully registered!"}).send();
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  
-  
+  const { user } = request;
+  const { title , deadline } = request.body
+  const { id } = request.params
+  const todo = user.todo.find(todo => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({ message: "Task not found!"}).send()
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline)
+
+  return response.status(200).json({message: "task updated successfully!" })
   
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params
+  const todo = user.todo.find(todo => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({ message: "Task not found!"}).send()
+  }
+
+  todo.done = true
+  return response.status(200).json(todo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { id } = request.params
+
+  const todoIndex = user.todo.findIndex(todo => todo.id === id)
+
+  if(todoIndex === -1){
+    return response.status(404).json({ message: "Task not found!"}).send()
+  }
+
+  user.todo.splice(todoIndex, 1)
+
+  return response.status(204).send()
+
 });
 
-module.exports = app;
+module.exports = app
